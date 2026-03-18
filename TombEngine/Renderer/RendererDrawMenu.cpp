bcabd4a52dcb3106ead214c862f5eee524033cb7
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2026-03-18 19:47:29 -0400

    Fix epsilon

--- TombEngine/Renderer/RendererDrawMenu.cpp
+++ TombEngine/Renderer/RendererDrawMenu.cpp
@@ -983,17 +983,15 @@ namespace TEN::Renderer
 			return;
 
 		float alpha = GetInterpolationFactor();
+		auto color = item.GetInterpolatedColor(alpha);
+
+		if (color.A() <= EPSILON)
+			return;
 
 		auto objectNumber = item.GetObjectID();
 		auto pos = item.GetInterpolatedPosition(alpha);
 		auto orient = item.GetInterpolatedOrientation(alpha);
 		auto scale = item.GetInterpolatedScale(alpha);
-		auto color = item.GetInterpolatedColor(alpha);
-
-		constexpr float ALPHA_EPSILON = 1e-5f;
-		if (color.w <= ALPHA_EPSILON)
-			return;
-
 		int meshBits = item.GetMeshBits();
 
 		unsigned int stride = sizeof(Vertex);
